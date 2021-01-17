92ca52869db2373985f5f540699a7dfd26a3d559
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2021-01-17 19:14:38 +0300

    Add files via upload

--- obj.js
+++ obj.js
@@ -23,5 +23,31 @@ function animal(name,color,legs){
  //https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
 
 
-  
+ 'use strict';
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  console.log(d1+a2+d2);
+ 
+}
+
+function Bee(){
+  //select some variable to combine "Bee"
+  console.log(b1+e2+e2);
+}
+
+function banana(){
+  //select some variable to combine "banana"
+  console.log(b2+a2+n2+a2+n2+a2);
+}
+Dad();
+Bee();
+banana();
+
+//https://www.codewars.com/kata/571edea4b625edcb51000d8e/train/javascript
+
+
+
+
   
